4427d4600fbb27be70a9d2cc7a4e842d24f4449e
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-08-01 06:49:20 -0600

    Updated test.py

--- mne/test.py
+++ mne/test.py
@@ -2,5 +2,5 @@ import requests
 
 url  =  "http://127.0.0.1:5000"
 
-file = open(r"C:\Users\srosa\Documents\EEG_Cat_Study4_II_II_S1.bdf", "rb")
-print(requests.post(url, files={"eeg_file": file}).json())
+file = open(r"F:\src\nathacks2022\mne\s07.csv", "rb")
+print(requests.post(url, data={"duration": "60"},files={"eeg_file": file}).json())
