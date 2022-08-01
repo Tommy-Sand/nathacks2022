ed66c47e1213830da1f09f1678d5ab94f8521bac
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-31 21:35:15 -0600

    Created rest API to allow communication with process_eeg.py given a file via POST

--- mne/main.py
+++ mne/main.py
@@ -1,103 +1,31 @@
-from typing import Type
-import mne, os
-import numpy as np
-import pandas as pd 
+import flask
+from flask_restful import reqparse, Api, Resource, inputs
+import werkzeug
+from process_eeg import process_eeg
 from pathlib import Path
 
-def recording_length():
-    while(True):
-        try:
-            duration = float(input("How many seconds did it take to capture this data: "))
-        except ValueError:
-            print("Invalid input, please enter a number")
+UPLOAD_DIR = input("Enter a directory you want to use to store databases")
+
+app = flask.Flask(__name__)
+api = Api(app)
+
+class Upload(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+    def post(self):
+        self.parser.add_argument("eeg_file", type=werkzeug.datastructures.FileStorage, location="files")
+        parse_args = self.parser.parse_args()
+        eeg_file = parse_args.get("eeg_file")
+        duration = flask.request.form.get("duration")
+        path = Path(UPLOAD_DIR + eeg_file.filename)
+        eeg_file.save(path)
+        if(duration != None):
+            to_be_returned = process_eeg(path, duration)
         else:
-            break
-    return duration
+            to_be_returned = process_eeg(path)
+        path.unlink(missing_ok=True)
+        return to_be_returned
+api.add_resource(Upload, "/")
 
-def init_data(path):
-    return pd.read_csv(path).transpose()
-
-def init_raw(path):
-    try:
-        return mne.io.read_raw(path)
-    except FileNotFoundError:
-        print("File not found")
-        return 1
-    except AssertionError:
-        print("File not allowed format")
-        return 2
-    except:
-        print("Unknown error")
-        return 3
-
-def init_raw_from_data(data, duration):
-    #These channel names are meaningless since we will average all channels
-    misc_ch_names = [str(i) for i in range(len(data))]
-
-    num_rows = data.shape[1]
-
-    info = mne.create_info(misc_ch_names, num_rows/(duration), ch_types="eeg")
-
-    return mne.io.RawArray(data, info)
-
-def init_freq_dom(raw):
-    psds, freqs = mne.time_frequency.psd_welch(raw, tmax=60, fmax=140, n_jobs=-1, n_fft=2048)
-
-    psds = 10 * np.log10(psds)
-    psds_mean = psds.mean(0)
-    return freqs, psds_mean
-
-def collect_brain_waves(freqs, psd_mean):
-    psd_min = abs(min(psd_mean))
-    
-    dic_waves = {"delta" : 0, "theta" : 0, "alpha" : 0, "beta" : 0, "gamma" : 0}
-
-    i=0
-    while(i < len(freqs) and freqs[i] < 4):
-        dic_waves["delta"] += psd_mean[i] + psd_min
-        i+= 1
-
-    while(i < len(freqs) and freqs[i] < 8):
-        dic_waves["theta"] += psd_mean[i] + psd_min
-        i+=1
-
-    while(i < len(freqs) and freqs[i] < 13):
-        dic_waves["alpha"] += psd_mean[i] + psd_min
-        i+=1
-
-    while(i < len(freqs) and freqs[i] < 30):
-        dic_waves["beta"] += psd_mean[i] + psd_min
-        i+=1
-
-    while(i < len(freqs) and freqs[i] < 140):
-        dic_waves["gamma"] += psd_mean[i] + psd_min
-        i+=1
-
-    return dic_waves
-
-def main(path):
-    #we ask how long the recording is
-    #We initialize and load the data
-    #Perform verification to check if the columns and rows and everything are the correct length
-    #Transform data to raw
-    #Transform raw to frequency domain, and convert to db
-    #Calculate the most common brain waves
-    path = Path(path)
-    file_format = Path(path).suffix
-    duration = recording_length()
-    if(file_format == '.csv'):
-        data = init_data(path)
-        if(isinstance(data, int)):
-            return None
-        raw = init_raw_from_data(data, duration)
-    else:
-        raw = init_raw(path)
-        if(isinstance(raw, int)):
-            return None
-    freq, db_mean  = init_freq_dom(raw)
-    return collect_brain_waves(freq, db_mean)
-
-
-if(__name__ == '__main__'):
-    path = input("Enter a file that contains eeg data: ")
-    print(main(path))
+if(__name__ == "__main__"):
+    app.run(debug=True)
